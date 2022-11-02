68e49e7e996cbab2fcda084bba475f01c5d70157
gallottino <d.gallotti@outlook.it>
2022-11-02 17:25:00 +0100

    Add Segment in geometry module
    Signed-off-by: gallottino <d.gallotti@outlook.it>

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,2 +1,3 @@
 pub mod point;
 pub mod points;
+pub mod segment;
---
+++ src/geometry/segment.rs
@@ -0,0 +1,169 @@
+use std::fmt::Display;
+
+use super::point::Point;
+
+pub struct Segment {
+    pub start: Point,
+    pub end: Point,
+}
+
+impl Segment {
+    /// It is not important the order of p1 and p2
+    pub fn new(p1: impl Into<Point>, p2: impl Into<Point>) -> Self {
+        let start: Point = p1.into();
+        let end: Point = p2.into();
+        if start < end {
+            Self { start, end }
+        } else {
+            Self {
+                start: end,
+                end: start,
+            }
+        }
+    }
+
+    pub fn contains(&self, p: Point) -> bool {
+        f64::min(self.start.x, self.end.x) <= p.x
+            && p.x <= f64::max(self.start.x, self.end.x)
+            && f64::min(self.start.y, self.end.y) <= p.y
+            && p.y <= f64::max(self.start.y, self.end.y)
+    }
+
+    pub fn find_interpolation(s1: Segment, s2: Segment) -> Option<Point> {
+        let a1 = s1.end.y - s1.start.y;
+        let b1 = s1.start.x - s1.end.x;
+        let c1 = a1 * s1.start.x + b1 * s1.start.y;
+
+        let a2 = s2.end.y - s2.start.y;
+        let b2 = s2.start.x - s2.end.x;
+        let c2 = a2 * s2.start.x + b2 * s2.start.y;
+
+        let denominator = a1 * b2 - a2 * b1;
+        if denominator == 0.0 {
+            return None;
+        }
+
+        let x = (b2 * c1 - b1 * c2) / denominator;
+        let y = (a1 * c2 - a2 * c1) / denominator;
+
+        let interpolation_point = Point::from2d(x, y);
+        if s1.contains(interpolation_point) == false || s2.contains(interpolation_point) == false {
+            return None;
+        }
+
+        Some(interpolation_point)
+    }
+}
+
+impl Display for Segment {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{},{}]", self.start, self.end)
+    }
+}
+
+impl PartialEq for Segment {
+    fn eq(&self, other: &Self) -> bool {
+        self.start == other.start && self.end == other.end
+    }
+}
+
+impl PartialOrd for Segment {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self.start.partial_cmp(&other.start) {
+            Some(core::cmp::Ordering::Equal) => {}
+            ord => return ord,
+        }
+        self.end.partial_cmp(&other.end)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::point::Point;
+
+    use super::Segment;
+
+    #[test]
+    fn new_segment() {
+        let s = Segment::new([2.0, 2.0], [4.0, 4.0]);
+
+        assert_eq!(s.start, [2.0, 2.0].into());
+    }
+
+    #[test]
+    fn new_segment_reverse() {
+        let s = Segment::new([4.0, 4.0], [2.0, 2.0]);
+
+        assert_eq!(s.start, [2.0, 2.0].into());
+    }
+
+    #[test]
+    fn partial_eq_segment() {
+        todo!()
+    }
+
+    #[test]
+    fn segment_contains_point() {
+        let s = Segment::new([0.0, 0.0], [2.0, 2.0]);
+        let p = Point::from2d(2.0, 2.0);
+
+        assert!(s.contains(p));
+    }
+
+    #[test]
+    fn segment_constains_point_limits() {
+        let s = Segment::new([0.0, 4.0], [2.0, 2.0]);
+        let p = Point::from2d(2.0, 2.0);
+
+        assert!(s.contains(p));
+    }
+
+    #[test]
+    fn segment_doesnt_contains_point() {
+        let s = Segment::new([0.0, 0.0], [2.0, 2.0]);
+        let p = Point::from2d(3.0, 3.0);
+
+        assert_eq!(s.contains(p), false);
+    }
+
+    #[test]
+    fn find_interpolation() {
+        let s1 = Segment::new([0.0, -2.0], [0.0, 2.0]);
+        let s2 = Segment::new([-2.0, 0.0], [2.0, 0.0]);
+
+        let opt_point = Segment::find_interpolation(s1, s2);
+        assert!(opt_point.is_some());
+
+        let point = opt_point.unwrap();
+        assert_eq!(point, Point::from2d(0.0, 0.0));
+    }
+
+    #[test]
+    fn find_interpolation_parallel() {
+        let s1 = Segment::new([0.0, 0.0], [4.0, 0.0]);
+        let s2 = Segment::new([2.0, 0.0], [4.0, 0.0]);
+
+        assert_eq!(Segment::find_interpolation(s1, s2), None)
+    }
+
+    #[test]
+    fn find_interpolation_incident_none() {
+        let s1 = Segment::new([0.0, 0.0], [4.0, 4.0]);
+        let s2 = Segment::new([0.0, 4.0], [1.0, 3.0]);
+
+        assert_eq!(Segment::find_interpolation(s1, s2), None)
+    }
+
+    #[test]
+    fn find_interpolation_incident() {
+        let s1 = Segment::new([0.0, 0.0], [4.0, 4.0]);
+        let s2 = Segment::new([0.0, 4.0], [2.0, 2.0]);
+
+        assert!(s1.contains(Point::from2d(2.0, 2.0)));
+        assert!(s2.contains(Point::from2d(2.0, 2.0)));
+        assert_eq!(
+            Segment::find_interpolation(s1, s2).unwrap(),
+            Point::from2d(2.0, 2.0)
+        );
+    }
+}
